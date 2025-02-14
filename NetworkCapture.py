f7fac6f91ab4e31eb86d7478c259401848722220
Matteo <matteomollano@gmail.com>
2025-02-14 16:40:14 -0500

    NetworkCapture: added function to convert a packet to a dictionary

--- NetworkCapture.py
+++ NetworkCapture.py
@@ -1,5 +1,6 @@
 from scapy.all import sniff, IP, TCP
 from functools import partial
+import json
 
 def packet_callback(packet):
     # packet.show()
@@ -16,6 +17,21 @@ def filter_by_port(packet, port):
             return True
     return False
 
-filter_handler = partial(filter_by_ip, ip='192.168.3.74')
+def packet_to_dict(packet):
+    layers = {}
+    while packet:
+        layer_name = packet.name
+        layers[layer_name] = packet.fields
+        packet = packet.payload
+
+    return layers
+
+# filter_handler = partial(filter_by_ip, ip='192.168.3.74')
 # filter_handler = partial(filter_by_port, port=443)
-sniff(iface="en0", prn=packet_callback, lfilter=filter_handler, store=False)
+# sniff(iface="en0", prn=packet_callback, lfilter=filter_handler, store=False)
+
+packet = sniff(iface="en0", count=1)[0]
+packet_dict = packet_to_dict(packet)
+
+print(packet_dict)
+packet.show()
