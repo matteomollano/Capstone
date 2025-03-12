77fa0ff86e832d2e28b3891f27003cabdba74345
Matteo <matteomollano@gmail.com>
2025-03-12 19:05:37 -0400

    added precision, recall, and f1-score metrics to models

--- backend/ml/nb15_model2.py
+++ backend/ml/nb15_model2.py
@@ -2,10 +2,10 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-df1 = pd.read_csv("datasets/UNSW_NB15_training-set.csv")
-df2 = pd.read_csv("datasets/UNSW_NB15_testing-set.csv")
+df1 = pd.read_csv("../datasets/UNSW_NB15_training-set.csv")
+df2 = pd.read_csv("../datasets/UNSW_NB15_testing-set.csv")
 
 df1 = df1.drop(columns=['id'])
 df2 = df2.drop(columns=['id'])
@@ -46,6 +46,18 @@ y_pred = rf_model.predict(X_test)
 # accuracy
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy:.4f}")
+
+# precision
+precision = precision_score(y_test, y_pred)
+print(f"Precision: {precision}")
+
+# recall
+recall = recall_score(y_test, y_pred)
+print(f"Recall: {recall}")
+
+# f1-score
+f1 = f1_score(y_test, y_pred)
+print(f"F1-Score: {f1}")
 
 # feature importances
 importances = rf_model.feature_importances_
