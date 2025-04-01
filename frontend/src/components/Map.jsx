6d240ec1b0bc17828e7faacdde9071c0e6e18293
Matteo <matteomollano@gmail.com>
2025-04-01 19:45:57 -0400

    4/1: created Map component for domain geolocation info

---
+++ frontend/src/components/Map.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef, } from 'react';
+import mapboxgl from "mapbox-gl";
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+// setting up access token
+const mapboxToken = process.env.REACT_APP_MAPBOX_TOKEN
+if (typeof mapboxToken === "undefined") {
+    throw new Error("You need a mapbox token to run this application")
+}
+mapboxgl.accessToken = mapboxToken;
+
+export default function Map() {
+    // references to map container and the actual map
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+
+    // coordinates (will be updated to work with api data)
+    const latitude = 40.7169 // using hofstra coordinates for testing
+    const longitude = -73.5990
+
+    useEffect(() => {
+        if (!map.current && mapContainer.current) {
+            map.current = new mapboxgl.Map({
+                container: mapContainer.current,
+                attributionControl: true,
+                center: [longitude, latitude],
+                zoom: 11.5,
+                pitch: 50,
+                bearing: -45,
+                style: 'mapbox://styles/mapbox/standard',
+            });
+        }
+
+        map.current.on('style.load', () => {
+            // set the light preset to be in dusk mode
+            map.current.setConfigProperty('basemap', 'lightPreset', 'dusk');
+        });
+
+    }, [longitude, latitude]);
+
+    return (
+        <div className="map-component" ref={mapContainer} />
+    )
+}
