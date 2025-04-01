bc69094266616f52e2ff6a7817b09cc3c58aab9e
Matteo <matteomollano@gmail.com>
2025-04-01 19:46:19 -0400

    4/1: tested Map component on home page for now

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,10 @@
+import Map from "../components/Map"
+
 export default function Home(){
-    return <h1>Home Page</h1>
+    return (
+        <>
+            <h1>Home Page</h1>
+            <Map />
+        </>
+    )
 }
