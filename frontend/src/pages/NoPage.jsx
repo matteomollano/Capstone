78771bbf38242af2f7ec4e6f9fedbb08ec1d1769
Matteo <matteomollano@gmail.com>
2025-04-11 18:18:25 -0400

    updated 404 page

--- frontend/src/pages/NoPage.jsx
+++ frontend/src/pages/NoPage.jsx
@@ -1,3 +1,31 @@
+import { Link } from 'react-router-dom';
+
 export default function NoPage() {
-    return <h1>404 Page Not Found</h1>
+    return (
+        <div className="no-page-container">
+
+            <section className="left-section">
+                
+                <div className="error-message">
+                    <p className="error-title">Whoops . . .</p>
+                    <p className="error-description">That page was not found</p>
+                </div>
+
+                <div className="error-navigation">
+                    <p>Checkout some of our other pages here:</p>
+                    <ul className="navigation-list">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/tables">Tables</Link></li>
+                        <li><Link to="/graphs">Graphs</Link></li>
+                    </ul>
+                </div>
+
+            </section>
+
+            <section className="right-section">
+                <img src="404-page.webp" alt="page not found robot" />
+            </section>
+
+        </div>
+    )
 }
