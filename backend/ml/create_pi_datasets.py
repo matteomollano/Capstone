e0525ac9d01aa55223720dc3a325b56982189d79
Matteo <matteomollano@gmail.com>
2025-04-15 18:39:11 -0400

    replaced create_testset.py with create_pi_datasets.py; this now creates both the train and test sets for pi network data; added flow_id, src_port, dst_port, protocol, and num_packets columns to datasets

--- backend/ml/create_pi_datasets.py
+++ backend/ml/create_pi_datasets.py
@@ -2,11 +2,16 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from database.utils import get_db_connection
 import pandas as pd
+# pd.set_option('display.max_columns', None)
 
 with get_db_connection() as conn:
     with conn.cursor() as cursor:
         query = '''
         SELECT
+            flow_id,
+            src_port as sport,
+            dst_port as dport,
+            protocol as proto,
             ttl_src as sttl,
             ttl_dst as dttl,
             ttl_states as ct_state_ttl,
@@ -18,15 +23,17 @@ with get_db_connection() as conn:
             mean_size_dst as dmean,
             rate,
             end_time - start_time as dur,
+            num_packets,
             is_malicious as label
         FROM Flows
         '''
         cursor.execute(query)
         results = cursor.fetchall()
-        column_names = ['sttl', 'dttl', 'ct_state_ttl', 'sload', 'dload', 'sbytes', 'dbytes', 'smean', 'dmean', 'rate', 'dur', 'label']
+        column_names = [desc[0] for desc in cursor.description]
 
         # load results into a dataframe
         data = pd.DataFrame(results, columns=column_names)
+        data = data[data.dur <= 60]
         
         # convert duration to float
         data['dur'] = data['dur'].astype(float)
@@ -34,8 +41,14 @@ with get_db_connection() as conn:
         # convert ct_state_ttl list to its length
         data['ct_state_ttl'] = data['ct_state_ttl'].str.split(",").apply(lambda x: len(x) if x is not None else 0)
         
-        # fill NaN values in sttl and dttl with 0 (from ARP and ICMP flows)
-        data[['sttl', 'dttl']] = data[['sttl', 'dttl']].fillna(0)
+        # fill NaN values in sport, dport, sttl, and dttl with 0 (from ARP and ICMP flows)
+        data[['sport', 'dport', 'sttl', 'dttl']] = data[['sport', 'dport', 'sttl', 'dttl']].fillna(0)
         
-        # save data to csv file
-        data.to_csv('real_data_test.csv', index=False) 
+        benign_data = data[data.label == 0]
+        malicious_data = data[data.label == 1]
+        
+        # save benign data to a train set csv file
+        benign_data.to_csv('real_pi_data.csv', index=False)
+        
+        # save malicious data to a test set csv file
+        malicious_data.to_csv('malicious_flows.csv', index=False)
