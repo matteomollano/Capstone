adacb669913496a5db60fc863beb2212fc34ace4
Matteo <matteomollano@gmail.com>
2025-04-22 19:44:18 -0400

    last week: used knn for last anomaly detection model; used Random Forest to create a binary classification model for the synthetic ddos attacks

---
+++ backend/ml/anomaly-detection/pi_knn.py
@@ -0,0 +1,130 @@
+import pandas as pd, numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sys
+sys.path.append("../../")
+from utils.protocols import get_protocol_number
+from sklearn.metrics import classification_report
+
+# load home pi dataset
+train = pd.read_csv("../../datasets/real_pi_data.csv")
+# convert protocol name to protocol number
+train['proto'] = train['proto'].apply(get_protocol_number)
+
+# get features and label for train set
+X_train = train.drop(columns=['flow_id', 'label'])
+y_train = train['label']
+
+# load test data set (malicious flows)
+test = pd.read_csv("../../datasets/malicious_flows.csv")
+# convert protocol name to protocol number
+test['proto'] = test['proto'].apply(get_protocol_number)
+
+# get features and label for test set
+X_test = test.drop(columns=['flow_id', 'label'])
+y_test = test['label']
+
+
+
+# normalize features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+
+# function to visualize anomaly scores distribution
+def anomaly_scores_dist(n_neighbors, train_anomaly_scores, test_anomaly_scores):
+    # plot train anomaly scores
+    sns.histplot(train_anomaly_scores, color='lightblue', label='Train Scores', stat='density', bins=20)
+
+    # plot test anomaly scores
+    sns.histplot(test_anomaly_scores, color='green', label='Test Scores', stat='density', bins=20)
+
+    # plot the threshold as a vertical line
+    plt.axvline(x=threshold, color='red', linestyle='--', label=f'Threshold: {threshold:.2f}')
+
+    # labels and title
+    plt.xlabel('Anomaly Score')
+    plt.ylabel('Density')
+    plt.title(f'Anomaly Score Distribution (Train vs Test) for k = {n_neighbors}')
+    plt.legend(loc='upper left')
+
+    # set y-axis limit to a reasonable range (e.g., between 0 and 2, since the density max is under 2)
+    plt.ylim(0, 0.005)
+
+    # show plot
+    plt.show()
+
+
+
+# model
+for n in [5, 10, 15, 20, 25, 30, 35, 40]:
+    print(f"\nFOR N = {n}")
+    knn = NearestNeighbors(n_neighbors=n)
+    knn.fit(X_train_scaled)
+
+    # determine a threshold for anomaly — for now, use the max distance from training set as cutoff
+    # (can also use a percentile, like 95th)
+    train_distances, _ = knn.kneighbors(X_train_scaled)
+    threshold = train_distances.mean(axis=1).max()
+    # threshold = np.percentile(train_distances.mean(axis=1), 99.95)
+
+
+    # === Detect anomalies in TRAIN set ===
+    print("Train set results:")
+    # use mean distance to k-nearest neighbors as anomaly score for training data
+    train_anomaly_scores = train_distances.mean(axis=1)
+
+    # predict anomalies
+    train_y_pred = (train_anomaly_scores > threshold).astype(int)
+
+    # count anomalies detected in train set
+    num_train_malicious = np.sum(train_y_pred == 1)
+    num_train_benign = np.sum(train_y_pred == 0)
+    print(f"[Train Set] Number of malicious flows: {num_train_malicious}")
+    print(f"[Train Set] Number of benign flows: {num_train_benign}")
+
+    # display anomalous records from train set
+    train_malicious_flows = train[train_y_pred == 1]
+    # print(f"[Train Set] Malicious flows:")
+    # print(train_malicious_flows)
+
+
+
+    # === Detect anomalies in TEST set ===
+    print("\nTest set results:")
+
+    # get distances to the k-nearest neighbors for each test point
+    test_distances, _ = knn.kneighbors(X_test_scaled)
+
+    # use the mean distance as the anomaly score
+    test_anomaly_scores = test_distances.mean(axis=1)
+
+    # predict anomalies
+    test_y_pred = (test_anomaly_scores > threshold).astype(int)
+
+    # count anomalies detected
+    test_num_malicious = np.sum(test_y_pred == 1)
+    test_num_benign = np.sum(test_y_pred == 0)
+    print(f"[Test Set] Number of malicious flows: {test_num_malicious}")
+    print(f"[Test Set] Number of benign flows: {test_num_benign}")
+
+    # display malicious records (where binary_pred == 1)
+    test_malicious_flows = test[test_y_pred == 1]
+    print(f"[Test Set] Malicious flows (anomalies):")
+    print(test_malicious_flows)
+    
+    
+
+    # display anomaly scores for train set vs test set
+    # (used to see how far outside of the train boundary the test malicious flows are)
+    print(f"\nTrain anomaly score range: {train_anomaly_scores.min()} to {train_anomaly_scores.max()}")
+    print(f"Test anomaly score range: {test_anomaly_scores.min()} to {test_anomaly_scores.max()}")
+    print(f"Threshold used: {threshold}")
+    print(f"Range difference: {test_anomaly_scores.max() - threshold}\n\n")
+    
+    # display anomaly scores dist plot
+    # anomaly_scores_dist(n, train_anomaly_scores, test_anomaly_scores)
---
+++ backend/ml/pi_random_forest.py
@@ -0,0 +1,103 @@
+import pandas as pd, numpy as np
+import sys
+sys.path.append("../")
+from utils.protocols import get_protocol_number
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+
+# load home pi dataset
+benign_df = pd.read_csv("../datasets/real_pi_data.csv")
+# convert protocol name to protocol number
+benign_df['proto'] = benign_df['proto'].apply(get_protocol_number)
+
+# load synthetically created malicious flows set
+malicious_df = pd.read_csv("../datasets/malicious_flows_updated.csv")
+
+# print("Benign flows:")
+# print(benign_df.head())
+
+# print("\nMalicious flows:")
+# print(malicious_df.head())
+
+data = pd.concat([benign_df, malicious_df], ignore_index=True)
+data = shuffle(data, random_state=42)
+
+X = data.drop(columns=['flow_id', 'label'])
+y = data['label']
+
+# split into train/test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# # train a random forest model
+# model = RandomForestClassifier(n_estimators=100, random_state=42)
+# model.fit(X_train, y_train)
+
+# # predictions
+# y_pred = model.predict(X_test)
+
+# # evaluations
+# print("Evaluations for first model (without hyperparameter tuning)")
+# print("Accuracy:", accuracy_score(y_test, y_pred))
+# print("Precision:", precision_score(y_test, y_pred))
+# print("Recall:", recall_score(y_test, y_pred))
+# print("F1 Score:", f1_score(y_test, y_pred))
+# print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+# print("Classification Report:\n", classification_report(y_test, y_pred))
+
+
+
+# HYPERPARAMETER TUNING
+rf = RandomForestClassifier(random_state=42)
+
+# define hyperparam grid
+param_grid = {
+    'n_estimators': [100, 150, 200, 250],
+    'max_depth': [None, 10, 20],
+    'min_samples_split': [2, 5],
+    'min_samples_leaf': [1, 2]
+}
+
+# Set up GridSearch with 5-fold CV
+grid_search = GridSearchCV(
+    estimator=rf,
+    param_grid=param_grid,
+    cv=5,          # 5-fold cross-validation (uses StratifiedKFold by default for balanced class splits)
+    scoring='f1',  # or 'accuracy', 'roc_auc', etc.
+    n_jobs=-1,     # use all CPU cores
+    # verbose=2
+)
+
+# fit on the full training set
+grid_search.fit(X_train, y_train)
+
+# best model and parameters
+print(f"Best params: {grid_search.best_params_}")
+print(f"Best score (CV): {grid_search.best_score_}")
+best_model = grid_search.best_estimator_
+
+
+# evaluate on train set
+y_pred_train = best_model.predict(X_train)
+
+# evaluate on test set
+y_pred_test = best_model.predict(X_test)
+
+print("Classification Report on train set:")
+print(classification_report(y_train, y_pred_train) + "\n")
+
+print("Classification Report on test set:")
+print(classification_report(y_test, y_pred_test))
+
+
+# feature importances
+importances = best_model.feature_importances_
+importances_df = pd.DataFrame({
+    'Feature': X_train.columns,
+    'Importance': importances
+})
+
+importances_df = importances_df.sort_values(by='Importance', ascending=False)
+print("\nFeature importances:")
+print(importances_df)
